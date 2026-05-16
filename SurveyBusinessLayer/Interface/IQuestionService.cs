7b4dd9d0c7a93f5af2370c73c37b0a718d88975a
sulieman Albuaeshi <soliman2020710@gmail.com>
2026-05-16 13:35:52 +0200

    update IQuestionRepository and IQuestionService to include surveyId in GetQuestionByIdAsync

--- SurveyBusinessLayer/Interface/IQuestionService.cs
+++ SurveyBusinessLayer/Interface/IQuestionService.cs
@@ -4,7 +4,7 @@ using Entities;
 public interface IQuestionService
 {
     Task<List<Question>> GetAllQuestionsAsync(int surveyId);
-    Task<Question?> GetQuestionByIdAsync(int id);
+    Task<Question?> GetQuestionByIdAsync(int id, int surveyId);
     Task<int> CreateQuestionAsync(int surveyid, Question que, IEnumerable<Choice> choice);
     Task<int>  UpdateQuestionAsync(Question que); 
     Task<bool> DeleteQuestionAsync(int id);
