25ce8aa66f078b4999e3cfc7d9b0eb9ce2227ccb
sulieman Albuaeshi <soliman2020710@gmail.com>
2026-05-25 00:35:07 +0200

    Refactor SQL query in AnswerRepository to improve answer retrieval and handle JSON serialization for answer values

--- SurveyDataAccessLayer/AnswerRepository.cs
+++ SurveyDataAccessLayer/AnswerRepository.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using DTOs;
 using Entities;
 using Microsoft.Data.SqlClient;
@@ -11,18 +12,19 @@ public class AnswerRepository : IAnswerRepository
     {
         await using var SqlConnection = new SqlConnection(DbHelperLocal.GetConnectionString());
         using var sqlcommand = new SqlCommand(@"
-                SELECT A.Id, Q.QuestionText, Q.QuestionType, 
-                       value = CASE
-                            WHEN A.AnswerType = 3 THEN CAST(A.TextValue AS NVARCHAR(100))
-                            WHEN A.AnswerType = 6 THEN CAST(A.NumberValue AS NVARCHAR(100))
-                            ELSE ( SELECT STRING_AGG(CAST(ChoiceId AS NVARCHAR(100)), ',') WITHIN GROUP (ORDER BY RankOrder)
-                                   FROM AnswerSelections ASel
-                                   WHERE ASel.AnswerId = A.Id ) 
-                            END
-                FROM Answers as A
-                    join dbo.Response R2 on R2.Id = A.ResponseId
-                    join dbo.Questions Q on Q.Id = A.QuestionId
-                where R2.SurveyId  = @SurveyId and userId = @UserId" , SqlConnection);
+                            SELECT A.Id, Q.QuestionText, A.AnswerType,
+                                   value = IIF(A.AnswerValue is not null, A.AnswerValue,            
+                                   (SELECT ROW_NUMBER() OVER (ORDER BY RankOrder) AS [id],
+                                           ChoiceId AS [value]
+                                    FROM AnswerSelections ASel
+                                    WHERE ASel.AnswerId = A.Id
+                                    ORDER BY RankOrder
+                                    FOR JSON PATH
+                                   ))
+                            FROM Answers as A
+                                     join dbo.Response R2 on R2.Id = A.ResponseId
+                                     join dbo.Questions Q on Q.Id = A.QuestionId
+                            where R2.SurveyId = @SurveyId  and userId = @UserId" , SqlConnection);
         sqlcommand.Parameters.AddWithValue("@SurveyId", surveyId);
         sqlcommand.Parameters.AddWithValue("@UserId", userId); // Assuming you have a userId parameter
 
@@ -30,14 +32,33 @@ public class AnswerRepository : IAnswerRepository
         var reader = await sqlcommand.ExecuteReaderAsync(); 
         
         var answers = new List<AnswerQuestionDto>();
+        object AnswerValue = null;
+        
         while (reader.Read())
         {
+            var value = reader.GetString(reader.GetOrdinal("value"));
+            var AnswerType = (QuestionType)reader.GetByte(reader.GetOrdinal("AnswerType"));
+            try
+            {
+                if(AnswerType == QuestionType.Checkbox || AnswerType == QuestionType.Rank)
+                {
+                    AnswerValue = JsonSerializer.Deserialize<object>(value);
+                }
+                else
+                {
+                    AnswerValue = value;
+                }
+            }
+            catch
+            {
+                AnswerValue = value;
+            }
             answers.Add(new AnswerQuestionDto
             {
                 Id = reader.GetInt32(reader.GetOrdinal("Id")),
                 QuestionText = reader.GetString(reader.GetOrdinal("QuestionText")),
-                AnswerType = (QuestionType)reader.GetInt32(reader.GetOrdinal("QuestionType")),
-                Value = reader.GetString(reader.GetOrdinal("Value"))
+                AnswerType = AnswerType,
+                Value = AnswerValue
             });
         }
 
